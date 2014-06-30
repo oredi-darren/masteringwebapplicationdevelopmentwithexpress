7a8c517aa84b1a8d426394abdefc27ec3795b75a
Darren Seet Serkai <darren@seet.me>
2014-06-30 17:49:28 +0900

    Started user model

--- chapter1/FileManager/models/user.js
+++ chapter1/FileManager/models/user.js
@@ -1,3 +1,23 @@
 /**
  * Created by darren on 6/28/14.
  */
+var mongoose = require('mongoose');
+var pass = require('pwd');
+
+var validateUser = function (username) {
+    return !!(username && /^[a-z][a-z0-9_-]{3,15}$/i.test(username));
+};
+
+var validatePassword = function (pass) {
+    return !!(pass && pass.length > 5)
+};
+
+var User = new mongoose.Schema({
+   username: {
+       type: String,
+       validate: validateUser,
+       unique: true
+   },
+   salt: String,
+   hash: String
+}, { safe: true});
