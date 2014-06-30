d144a99c3d7f9bf580f92be15ecbe25568e47d96
Darren Seet Serkai <darren@seet.me>
2014-06-30 17:38:54 +0900

    Completed file model

--- chapter1/FileManager/models/file.js
+++ chapter1/FileManager/models/file.js
@@ -1,3 +1,93 @@
 /**
  * Created by darren on 6/28/14.
  */
+var fs = require('fs');
+var async = require('async');
+var ROOT = __dirname + '/../files';
+var path = require('path');
+
+function File(userId, name) {
+    this.userId = userId;
+    this.name = name;
+    this.path = this._getPath();
+}
+
+File.prototype._getPath = function () {
+    return path.resolve(File.getUserPath(this.userId) + '/' + this.name);
+};
+
+File.prototype.isValidFileName = function () {
+    return /[a-z0-9_]/i.test(this.name);
+};
+
+File.prototype.exists = function (callback) {
+    if(!this.isValidFileName()) {
+        // keep the function async
+        return process.nextTick(function () {
+            callback(null, false);
+        });
+    }
+
+    fs.exists(this.path, callback);
+};
+
+File.prototype.delete = function (callback) {
+    this.exists(function (exists) {
+        if(!exists) { return callback(); }
+        fs.unlink(this.path, callback);
+    }).bind(this);
+};
+
+File.prototype.getStats = function (callback) {
+    fs.stat(this.path, callback);
+};
+
+File.prototype.save = function (tempPath, callback) {
+    if(!this.isValidFileName()) {
+        return process.nextTick(function () {
+            callback(null, new Error('Invalid filename'));
+        });
+    }
+
+    var readStream = fs.createReadStream(tempPath);
+    var writeStream = fs.createWriteStream(this.path);
+    // if an error occurs invoke the callback with an error param
+    readStream.on('error', callback);
+    writeStream.on('error', callback);
+    writeStream.on('close', callback);
+    readStream.pipe(writeStream);
+};
+
+File.getUserPath = function(userId) {
+    return ROOT + '/' + userId;
+};
+
+// create a folder if it doesn't exist already
+File.createFOlder = function (userId, callback) {
+    var userPath = FIle.getUserPath(userId);
+
+    fs.exists(userPath, function(exists) {
+        if(!exists) {
+            fs.mkdir(userPath, callback);
+        }
+    });
+};
+
+File.getByUserId = function (userId, callback) {
+    fs.readdir(File.getUserPath(userId), function (err, files) {
+        if(err) { return callback(err); }
+        if(!files) { return callback(null, []); }
+
+        // get the stats for every file
+        async.map(files, function (name, done) {
+           var file = new File(userId, name);
+            file.getStats(function (err, stats) {
+                if(err) { return done(err); }
+
+                done(null, { name: name, stats: stats })
+            });
+        }, callback);
+    });
+};
+
+exports = File;
